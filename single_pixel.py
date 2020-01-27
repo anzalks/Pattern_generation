e9c662227c0e3c1154ed55b12208a167e9441518
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-01-27 15:05:51 +0530

    Need a better solution.

--- single_pixel.py
+++ single_pixel.py
@@ -11,13 +11,16 @@ import itertools
 import tifffile
 import numpy as np
 
-H_, W_ = 11, 8
+H_, W_ = 20, 24
 res_ = np.zeros((H_,W_))
 
 
-def showFrame(mat, delay=1):
+def showFrame(pixel, delay=1):
     import cv2
     global res_
+    i, j = pixel
+    mat = np.zeros_like(res_)
+    mat[i, j] = 1
     res_ += mat
     res = np.hstack((mat, res_))
     cv2.imshow('window', cv2.resize(res, None, fx=5, fy=5))
@@ -25,45 +28,34 @@ def showFrame(mat, delay=1):
 
 def minDistance(f1, f2):
     # Both frames are guaranteed to have only 1 pixel.
-    x1, y1 = np.argwhere(f1>0)[0]
-    x2, y2 = np.argwhere(f2>0)[0]
+    x1, y1 = f1
+    x2, y2 = f2
     return ((x1-x2)**2 + (y1-y2)**2)**0.5
     
 
 def main():
-    global res_, H_, W_
-    Z = np.zeros_like(res_)
-    i = random.randint(0, H_-1)
-    j = random.randint(0, W_-1)
-    Z[i,j] = 1
-    frames = [Z]
-    while True:
+    global res_, H_, W_ Z = np.zeros_like(res_) i = random.randint(0, H_-1)
+    j = random.randint(0, W_-1) frames = [(i,j)] while True:
         if res_.min() > 0:
-            print("Whole space is covered")
-            break
-        #  frames.append(X)
-        # For how many frames it should not overlap. Continue with 2.
-        Z = np.zeros_like(res_)
-        i = random.randint(0, H_-1)
-        j = random.randint(0, W_-1)
-        Z[i, j] = 1
-        badFrame = False
-        for f in frames[-30:-1]:
-            d = minDistance(f, Z)
-            if d < 2**0.5:
-                print('☠', end=''); sys.stdout.flush()
-                badFrame = True
-                break
+            print("Whole space is covered") break
+        #  frames.append(X) # For how many frames it should not
+        overlap. Continue with 2.  i = random.randint(0, H_-1) j =
+        random.randint(0, W_-1) if (i, j) in frames:
+            print(end='.')  continue
+        badIndex = False for (x2,y2) in frames[-20:-1]:
+            d = minDistance((i, j), (x2,y2)) if d < 2**0.5:
+                print('☠', end=''); sys.stdout.flush() badIndex = True break
 
-        if not badFrame:
-            frames.append(Z)
-            showFrame(Z, 1)
+        if not badIndex:
+            frames.append((i,j))
+            showFrame((i,j), 1)
 
     # Write them to a tiff file.
     outfile = 'single_pixel.tiff'
     with tifffile.TiffWriter(outfile, imagej=True) as tif:
-        for frame in frames:
-            frame[frame==1] = 255
+        for i, j in frames:
+            frame = np.zeros_like(res_)
+            frame[i, j] = 255
             tif.save(np.uint8(frame))
     print( f"[INFO ] Saved to {outfile}" )
 
